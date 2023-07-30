0b7fa3ec3e22248be29b99e9b0dbee8907eaa381
Lilisalimi <86179008+Lilisalimi@users.noreply.github.com>
2023-07-30 15:28:18 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x5f097196b9d5e7aff2e55d63805acd01a415b060";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
